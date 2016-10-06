86f9289c4e7e74205a9de117d17d302e23f273c7
Ryan Ore <ryanore@jellyvision.com>
2016-10-06 08:12:15 -0500

    Basic running app

---
+++ server.js
@@ -0,0 +1,33 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 4000;
+var webpackConfig = require('./webpack.config');
+
+(function() {
+
+  var webpack = require('webpack');
+  var compiler = webpack(webpackConfig);
+
+  app.use(require("webpack-dev-middleware")(compiler, {
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath
+  }));
+
+  app.use(require("webpack-hot-middleware")(compiler));
+})();
+
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + '/index.dev.html');
+});
+
+
+app.listen(port, '0.0.0.0', function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log('Listening at http://localhost: ' + port);
+});
